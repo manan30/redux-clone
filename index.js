d5f844b1aed53e6eb984f24a16dccba6d3e5b500
Manan Joshi <mananjoshi1995@gmail.com>
2020-08-11 20:34:21 -0400

    Implementing the createStore function

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
 
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(action);
+    listeners.forEach((listener) => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
+
+export { createStore };
